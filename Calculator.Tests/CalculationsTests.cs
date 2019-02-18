aff5669ba3a228aac4867498e5a9ff27d5bf100f
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-02-18 17:52:39 +0100

    First Unit test

---
+++ Calculator.Tests/CalculationsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculationsTests
+    {
+        [Test]
+        [TestCase(2, 2, 4)]
+        [TestCase(2, 3, 5)]
+        [TestCase(2, 4, 6)]
+        [TestCase(2.5, 1, 3.5d)]
+        public void Add_AddsTwoDouble_ReternsInt(double a, double b, double c)
+        {
+            Calculations calculations = new Calculations();
+            Assert.AreEqual(c, calculations.Add(a, b));            
+        }
+    }
+}
